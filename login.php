a372bcea84cf07abe042e1daaf7514573d0c546f
monicamarisetty <mmari016@odu.edu>
2016-10-18 17:42:09 -0400

    back to normal

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+
+$clientId="725e08a43209c83e9833";
+$clientSecrect ="";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Milestone 1</title>
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<h1>Welcome to Questions & Answers Webpage </h1>
+<body>
+
+<div class="login">
+<div class="olduser">
+<form name="form1" method="post" action="Login_Verification.php" accept-charset="UTF-8">
+<h3>Login here</h3>
+    </br>
+    </br>
+<strong>Username:</strong>
+<input name="username" type="text" id="username" placeholder="Enter username">
+<strong>Password:</strong>
+<input name="password" type="password" id="password" placeholder="Enter password">
+</br></br>
+<input type="submit" name="Submit" value="Login">
+</form>
+</tr>
+</div>
+</div>
+
+</body>
+</html>
+
+
+	
