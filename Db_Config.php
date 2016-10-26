b5554ac45f7c222caeebecd6a0f8f31c25fe78ba
monicamarisetty <mmari016@odu.edu>
2016-10-26 13:08:31 -0400

    changed password

--- Db_Config.php
+++ Db_Config.php
@@ -2,7 +2,7 @@
 	//Used to connect to DB. Included once on every php page that needs access to DB
 	$servername = "localhost";
 	$username = "admin";
-	$password = "Mon@rch$";
+	$password = "M0n@rch$";
 	$dbname = "thrones";
 
 ?>
