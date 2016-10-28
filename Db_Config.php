ebc1fa8f83a5c16d15df54b96cd8054863b3336e
veena talapaneni <vtalapan@odu.edu>
2016-10-28 14:36:45 -0400

    delete

--- Db_Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//Used to connect to DB. Included once on every php page that needs access to DB
-	$servername = "localhost";
-	$username = "admin";
-	$password = "M0n@rch$";
-	$dbname = "thrones";
-
-?>
