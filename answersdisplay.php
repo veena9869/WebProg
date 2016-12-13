a349576320c8923cbc7e39e98fa0a5ee4338654d
monicamarisetty <mmari016@odu.edu>
2016-12-13 13:21:23 -0500

    tag chages

--- answersdisplay.php
+++ answersdisplay.php
@@ -54,6 +54,7 @@ function freeze() {
 				$bbcode = new BBCode;
 				$questionID = $_GET['var'];
 				$_SESSION['questionNum'] = $questionID;				
+				$conn = new mysqli($servername, $username, $password, $dbname);
 				if ($conn->connect_error ) {
 					die("Connection failed: " . $conn->connect_error);
 				}
@@ -73,7 +74,19 @@ function freeze() {
                 echo'
                         <div class="col-xs-12 col-sm-8" >                    
                             <div class = "content" style = "color:black">Question Description- '.html_entity_decode($bbcode->Parse($row['q_content'])) .'</div>
-                            <div class = "asker" style = "color:black">  Posted By- '. $row['q_asker'] .'</div>                    
+                            <div class = "asker" style = "color:black">  Posted By- '. $row['q_asker'] .'<br>';
+    $tags= $row['tags'];
+                                    $newtags=explode("#",$tags);
+                                    $tagcount=count($newtags);
+                  
+                  if($tagcount!=1){
+                                    while ($tagcount!=0)
+                                    {
+                                        $tagcount=$tagcount-1;
+                                        echo '<button style ="margin:10px;height:25px;width:40px;background-color:green;border:none;text-align:center;cursor:pointer;border-radius: 8px;"> <a href ="tags.php" style="color:white">'.$newtags[$tagcount].'</a></button>';
+                                    }
+                  }
+    echo '</div>                    
                         </div>';
                    if($_SESSION['username']!= null ){     
                        echo' 
@@ -81,16 +94,13 @@ function freeze() {
                             <form method="post" action="voteQuestion.php">
 							     <input type="submit" name="up" value="up">
 							     <input type="hidden" name="id" value="'.$questionID . '">
-                                 <input type="hidden" name="uid" value="'.$id . '">
-                                 <input type="hidden" name="asker" value="'.$row['q_asker'] . '">
-                                 
+                                 <input type="hidden" name="uid" value="'.$uid . '">
 				            </form>votes 
                             '.$row['q_value'].'
                             <form method="post" action="voteQuestion.php">
 							     <input type="submit" name="down" value="down">
 							     <input type="hidden" name="id" value="'.$questionID . '">
-                                 <input type="hidden" name="uid" value="'.$id . '">
-                                 <input type="hidden" name="asker" value="'.$row['q_asker'] . '">
+                                 <input type="hidden" name="uid" value="'.$uid . '">
 							 </form>
                              
                         </div>
@@ -134,14 +144,15 @@ function freeze() {
             $resultans = $conn->query($sqlanswers);
             $rowans = mysqli_fetch_array($resultans);
             $anscount=$rowans['anscount']/2;
-       
+        echo"anscount=".$anscount;
         $page=0;
             
             while( $i<$anscount)
             {
                 $i=$i+1;
-               echo'<ul class="pagination" style="margin-top:6em;">
-    <li><a href="anspagin.php? var1='.$i.'&var='.$questionID.'">'.$i.'</a></li></ul>';
+               echo' <ul class="pagination">
+    <li><a href="anspagin.php? var1='.$i.'&var='.$questionID.'">'.$i.'</a></li>    
+  </ul>';
                 
             }
 			$sql2 = "SELECT * FROM question JOIN answer ON question.q_id = answer.a_id
@@ -184,7 +195,7 @@ function freeze() {
             }
                 
                echo' <div class="col-xs-12 col-sm-8" style="text-align:center">
-							              <div style="color:black">'. html_entity_decode($row['a_content']) .'</div>
+							              <div style="color:black">'. html_entity_decode($bbcode->Parse($row['a_content'] )).'</div>
                                           <div style="color:black">-'. $row['a_asker'] .'</div>
 			                  </div>
                               <div class = "col-xs-12 col-sm-2">';
@@ -208,7 +219,8 @@ function freeze() {
             echo'</div>
             </div>
             </div>
-            </div>';  
+            </div>';
+            
         }			
 		?>
     </div>
